edf6645557495ab286e0836c5e8ff135640f4ec6
Eric Ross <epr@rossmbox.com>
2024-01-10 15:59:01 -0800

    Added response

--- section3/app.js
+++ section3/app.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
     //process.exit();
+    res.setHeader('Content-Type','text/html' );
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<boday<h1>Hello from Node.js server</h1></body');
+    res.write('</html>');
+    res.end();
 });
-
 server.listen(3000);
