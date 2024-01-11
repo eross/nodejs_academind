e974b58e5d5a5ae5fdaa2385f1ee5fe19e048359
Eric Ross <epr@rossmbox.com>
2024-01-11 14:36:15 -0800

    Add form

--- section3/app.js
+++ section3/app.js
@@ -3,11 +3,19 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     //process.exit();
+    if(req.url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html' );
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<boday<h1>Hello from Node.js server</h1></body');
     res.write('</html>');
     res.end();
+ 
 });
 server.listen(3000);
