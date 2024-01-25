cf1b0c463a38ca7a795ab9200565bc2d9ccdafe2
Eric Ross <epr@rossmbox.com>
2024-01-25 15:22:30 -0800

    Add middle ware and filters

--- section5/app.js
+++ section5/app.js
@@ -4,12 +4,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
+//always runs
+app.use('/',(req, res, next)=>{
+    console.log('This always runs');
     next();
+})
+app.use('/add-product',(req, res, next) => {
+    console.log('In add-product');
+    res.send('<h1>Hello from add-product</h1>');
 });
 
-app.use((req, res, next) => {
+// never runs
+app.use('/',(req, res, next) => {
     console.log('In another  middleware');
     res.send('<h1>Hello from express</h1>');
 });
