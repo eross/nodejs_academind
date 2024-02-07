65545ac2c6afa67b9403c884f78f2723001e46c7
Eric Ross <epr@rossmbox.com>
2024-02-07 09:48:38 -0800

    Add 404 page

--- section5b/app.js
+++ section5b/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,7 +9,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, 'views', 'err404.html'));
+});
 
 app.listen(3000);
